de12eeca6086c82f15fbe556b459c78a49a6a70b
ecrawford-0 <emily.crawford@mymail.champlain.edu>
2021-03-08 20:12:47 -0500

    added ssh-securing script

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+# secure-ssh.sh
+# author emily crawford
+# creates a new ssh user using the $l parameter
+# adds a public key from the local repo or curled from the remote repo
+# removes root's ability to ssh in
+echo "ALL CODE GOES HERE"
