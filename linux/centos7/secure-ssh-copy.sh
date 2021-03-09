ea80e85b9ec115882f4fc8e1339fe3a86dd1a363
ecrawford-0 <emily.crawford@mymail.champlain.edu>
2021-03-09 15:38:50 -0500

    hopefuly fixed the script

--- linux/centos7/secure-ssh-copy.sh
+++ linux/centos7/secure-ssh-copy.sh
@@ -14,8 +14,29 @@ sudo useradd -m -d /home/$user -s /bin/bash $user
 # create the home directory for the new user
 sudo mkdir /home/$user/.ssh
 
-#download the public key and place it in user's folder
-sudo cp ../public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+echo " $user has been created, now generating keys"
+# generate the key with no password and put the files in the .ssh directory
+sudo ssh-keygen -t rsa -q -N "" -f /home/$user/.ssh/$user
+
+# upload the public key to github
+sudo cp /home/$user/.ssh/$user.pub ../public-keys/$user.pub
+cd ../../
+git add .
+
+echo "enter the email for github"
+read email
+echo "enter the username for github"
+read username
+
+git config user.email $email
+git config user.name $username
+
+git commit -m "adding the public key for $user to login onto $(hostname)" 
+git status
+git push
+
+# copy the private key to authorized keys
+sudo cp /home/$user/.ssh/$user /home/$user/.ssh/authorized_keys
 
 # change permissions of .ssh file to read,write,and execute to owner of file, only 
 sudo chmod 700 /home/$user/.ssh
