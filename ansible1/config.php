f471227d7ceec437b88794eb46b36e359e108fd1
ecrawford-0 <emily.crawford@mymail.champlain.edu>
2021-03-17 02:23:38 +0000

    config file for nextcloud on ansible1

---
+++ ansible1/config.php
@@ -0,0 +1,31 @@
+<?php
+$CONFIG = array (
+  'passwordsalt' => 'ilvv/mc/VGFQl59/yvFxNZ61Jktnzo',
+  'secret' => 'VHGQVg07IeUvHavU/IzF18Vc0eOTsBAmclaQbbw1GKvzpNIT',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => 'ansible1-emily',
+  ),
+  'datadirectory' => '/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '21.0.0.18',
+  'overwrite.cli.url' => 'http://localhost',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
+  'installed' => true,
+  'instanceid' => 'ocgn4defcx1t',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'redis' => 
+  array (
+    'host' => '/var/run/redis/redis.sock',
+    'port' => '0',
+  ),
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
+  'logfile' => '/var/log/nextcloud/nextcloud.log',
+);
